cb002691c1ba36bf39a6d4a9a190855d508e2d68
root <root@instance-1.us-central1-c.c.bubbly-display-343803.internal>
2022-03-25 04:16:02 +0000

    Fri Mar 25 04:16:02 UTC 2022

--- git_backup.sh
+++ git_backup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "backup service is running"
+
+cd /data
+
 git config --system core.longpaths true
-git status
-git add /data/
-git commit -m "$(date)"
-git push origin main
+
+git status  > /data/git_backup.log		
+git add /data/ 	> /data/git_backup.log
+git commit -m "$(date)" > /data/git_backup.log
+git push origin main  > /data/git_backup.log
